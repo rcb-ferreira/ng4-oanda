03172139f858a86a23fd94bc057e8d429fc384b1
Rui Ferreira <ferreira.rcb@gmail.com>
2017-09-11 13:13:15 +0200

    v1.0.0

--- main.bundle.js
+++ main.bundle.js
@@ -81,11 +81,12 @@ var AuthenticatedComponent = (function () {
         this.candles = [];
     }
     AuthenticatedComponent.prototype.ngOnInit = function () {
+        this.setSplice = 0;
         this.accountId = this.user.getAccountId();
-        this.reset = false;
         this.loadingInstrument = true;
-        this.loadingCandles = false;
         this.loadingInstrument = false;
+        this.loadingCandles = false;
+        this.reset = false;
         this.granularities = ["S5", "S10", "S15", "S30", "M1", "M2", "M3", "M4", "M5", "M10", "M15", "M30", "H1", "H2", "H3", "H4", "H6", "H8", "H12", "D", "W", "M"];
         // Default instruments array to prevent jumping.
         this.instruments = [{ "name": "EUR_USD", "type": "CURRENCY", "displayName": "EUR/USD" }];
@@ -96,6 +97,10 @@ var AuthenticatedComponent = (function () {
         this.selectedGranularity = this.defaultGranularity;
         this.selectedInstrument = this.defaultInstrument;
         this.selectedCount = this.defaultCount;
+        // A quick and clean range counter to be used for dropdown.
+        for (var i = 10; i > 0; i--) {
+            this.countLimit.push(i * 10);
+        }
         this.token = this.user.getToken();
         this.listInstruments();
         this.getCandles(this.token, this.selectedInstrument, this.selectedGranularity, this.selectedCount);
